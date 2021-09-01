2e7a2630960b5507a8b44a1e7550270b591045bb
stanislauhvozdz <66819873+stanislauhvozdz@users.noreply.github.com>
2021-09-01 23:08:42 +0300

    push

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,11 @@
-try {
-  // бургер
-  let navigationToggle = document.querySelector('.navigation__toggle');
-  let navigationList = document.querySelector('.navigation__list');
-  let pageBody = document.querySelector('.page__body');
+let pageBody = document.querySelector('.page__body');
 
+try {
+  // бургер + меню
+  let navigationToggle = document.querySelector('.navigation__toggle');
+  navigationToggle.classList.remove('navigation__toggle--no-js');
+
+  let navigationList = document.querySelector('.navigation__list');
   navigationList.classList.remove('navigation__list--no-js')
 
   navigationToggle.addEventListener('click', (event) => {
@@ -15,6 +17,20 @@ try {
   });
 } catch {};
 
+
+let navigationLinks = document.querySelectorAll('.navigation__link');
+let navigationList = document.querySelector('.navigation__list');
+
+
+for (let link of navigationLinks) {
+  link.addEventListener('click', () => {
+    navigationList.classList.remove('is-active');
+    pageBody.classList.remove('_no-scroll');
+  })
+}
+
+
+//  Видео
 let buttonPlay = document.querySelector('.video__button-play');
 let video = document.querySelector('.video__player video')
 
@@ -27,3 +43,18 @@ buttonPlay.addEventListener('click', () => {
     buttonPlay.classList.remove('play')
   }
 });
+
+// Плавные якорные ссылки
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
